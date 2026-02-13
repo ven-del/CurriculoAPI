1593beea8b131ce485fd354eb4fb3d8785b80c08
ven-del <rwendell.regis@gmail.com>
2026-02-13 10:40:49 -0300

    feat: atualização da base de dados

---
+++ Migrations/20260213134014_AdicaoCargoProfEMudancaApresentacao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CurriculoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicaoCargoProfEMudancaApresentacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Educacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Curso = table.Column<string>(type: "text", nullable: false),
+                    CursoEn = table.Column<string>(type: "text", nullable: false),
+                    CursoEs = table.Column<string>(type: "text", nullable: false),
+                    Instituicao = table.Column<string>(type: "text", nullable: false),
+                    DataInicio = table.Column<DateOnly>(type: "date", nullable: false),
+                    DataFim = table.Column<DateOnly>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Educacoes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Educacoes");
+        }
+    }
+}
